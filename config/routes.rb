2d9c52cd6b2d44cd9d1641b086bb2be8e5b56421
theblackcat09400theblackcat102 <zhirui09400@hotmail.com>
2016-08-02 16:24:53 +0800

    fix footer floating

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins, :skip => [:registrations, :password]
   root 'posts#new'
-  get '/form' => 'posts#new'
   resources :posts do
     resources :comments
   end
+  get '/form' => 'posts#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
